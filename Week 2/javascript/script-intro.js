a894a838a1c4e0e9059c77ccf7fa52fad7bdd00e
Tim Mclennan <timmymclennan@gmail.com>
2022-11-16 08:37:29 +1100

    Added notes for javascript

---
+++ Week 2/javascript/script-intro.js
@@ -0,0 +1,18 @@
+/* Challenge: Storing Personal Information
+
+### MVP:
+
+1. Create three variables to store your first name, last name and age.
+
+
+1. Assign values to these variables.
+1. Print a string containing all three variables in the console (should look like "Hi, my name is XXX XXX and I am XX years old) */
+
+const firstName = "Tim";
+const lastName = "Mclennan";
+
+let age = 25;
+
+console.log(
+    "Hi, my name is" + firstname + lastName + "and I am" + age " years old."
+);
